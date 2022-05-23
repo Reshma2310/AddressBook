3f8eb3949621398ca543979e93d0ef7f8bbc1aa4
Reshma2310 <dreshma253@gmail.com>
2022-05-23 15:08:14 +0500

    [Reshma] - Creating Contacts in Address Book

---
+++ AddressBook/AddressBook/Contacts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
